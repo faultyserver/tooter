28cc609ff2d55074355f6ee353765b7ca4d3b304
Jon <audiobahn404@gmail.com>
2016-09-10 18:12:49 -0400

    Basic Toot and User validations Toots can't be blank (you have to say *something* in your toot), and User's handles must be unique (how else can you be identified nicely?).

--- app/models/toot.rb
+++ app/models/toot.rb
@@ -1,3 +1,5 @@
 class Toot < ApplicationRecord
   belongs_to :author, class_name: 'User', inverse_of: :toots
+
+  validates_presence_of :body
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :toots, foreign_key: 'author_id', inverse_of: :author
 
+  validates_uniqueness_of :handle
+
   has_secure_password
 end
