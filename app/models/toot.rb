e70ee2343fa8d23539716df1e13f9e46acf45aeb
Jon <audiobahn404@gmail.com>
2016-09-10 23:30:15 -0400

    Pass all model tests Toot relations are now ordered by descending creation date by default (`default_scope`).
    User now has appropriate validations for handle, name, bio (non-nil), and password (length >= 8).

--- app/models/toot.rb
+++ app/models/toot.rb
@@ -1,5 +1,7 @@
 class Toot < ApplicationRecord
   belongs_to :author, class_name: 'User', inverse_of: :toots
 
+  default_scope { order(created_at: :desc) }
+
   validates_presence_of :body
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :toots, foreign_key: 'author_id', inverse_of: :author
 
   validates_uniqueness_of :handle
+  validates_presence_of :handle, :name, :password
+  validates :bio, length: { in: 0..200 }, allow_nil: false
+  validates :password, length: { minimum: 8 }
 
   has_secure_password
 end
