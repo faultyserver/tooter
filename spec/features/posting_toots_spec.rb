f97c57ff0a0cc67644a037dd58481c9190a6299e
Jon <audiobahn404@gmail.com>
2016-09-12 15:23:25 -0400

    Add "Posting Toots" feature spec. The spec covers the (few) cases related to posting toots: when users are not signed in, they are redirected to the sign in page; when users are logged in, only valid toots are allowed.

---
+++ spec/features/posting_toots_spec.rb
@@ -0,0 +1,52 @@
+feature 'Posting toots' do
+  let(:toot_body) { 'toot toot.' }
+
+  scenario 'while not signed in' do
+    visit new_toot_path
+    expect_sign_in_request
+  end
+
+  context 'while signed in' do
+    before(:each) do
+      @user = create(:user)
+      sign_in_as @user.handle, @user.password
+    end
+
+    scenario 'with a valid body' do
+      attempt_toot body: toot_body
+      expect_toot_success
+    end
+
+    scenario 'with a blank body' do
+      attempt_toot body: ''
+      expect_toot_failure
+    end
+  end
+
+
+  def attempt_toot body:
+    visit new_toot_path
+    fill_in 'toot_body', with: body
+    click_button 'Create Toot'
+  end
+
+  def expect_toot_failure
+    expect(page).to have_content('can\'t be blank')
+  end
+
+  def expect_toot_success
+    expect(page).to have_content(@user.handle)
+    expect(page).to have_content(toot_body)
+  end
+
+  def expect_sign_in_request
+    expect(page).to have_current_path(login_path)
+  end
+
+  def sign_in_as handle, password
+    visit login_path
+    fill_in 'Handle',   with: handle
+    fill_in 'Password', with: password
+    click_button 'Login'
+  end
+end
