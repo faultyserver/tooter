a7b44bef6871f6867db85041dc79a73b5f87206e
Jon <audiobahn404@gmail.com>
2016-09-13 19:15:09 -0400

    Initial @mentioning functionality @mentions are now mostly supported. Templates can use the `with_mentions` helper to scan a block of text and replace any @mentions with links to the respective user profiles.
    However, the implementation is very poor, and would be much better done by a simple JS script. This, though, requires that profile URLs be fully determinable from a user's handle, which it currently is not (URLs require User ids). Attempting to make vanity URLs that use the user's handle instead was successful, but ended up regressing a number of tests, so the functionality was dropped for now.

--- app/helpers/toots_helper.rb
+++ app/helpers/toots_helper.rb
@@ -2,4 +2,16 @@ module TootsHelper
   def user_has_favorited(toot)
     current_user && current_user.events.where(action: 'favorite', subject: toot).exists?
   end
+
+  # Return the given string with any @mentions replaced with a link to that
+  # user's profile.
+  # NOTE: This is *definitely* not the best way to do this. Ideally, user
+  # handles would make valid URLs, and no lookups would be necessary. A JS
+  # script could simply wrap @mentions with `a` tags using the handle.
+  def with_mentions text
+    text.gsub(/@\w+/) do |handle|
+      puts handle
+      "<a class='at-mention' href=\"#{user_path(User.where(handle: handle[1..-1]).first)}\">#{handle}</a>"
+    end.html_safe
+  end
 end
