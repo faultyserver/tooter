8b49e232bd18fa6931a95f12b497b85174ab1383
Jon <audiobahn404@gmail.com>
2016-09-14 00:15:58 -0400

    Filter self-action events. Events where the user who initiated the action is also the owner of the subject (i.e., a Toot's author), should not be displayed in event streams, since the subject has most likely already been rendered during it's creation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
   def render_event event
-    render "events/#{event.action}", event: event
+    # Don't render events that meet specific criteria
+    if event.subject.is_a?(Toot) && event.user == event.subject.author
+      return unless event.action == 'toot'
+    end
+
+    # If none of those criteria were met, go ahead and render the event
+    content_tag(:div, class: 'event') do
+      render "events/#{event.action}", event: event
+    end
   end
 end
