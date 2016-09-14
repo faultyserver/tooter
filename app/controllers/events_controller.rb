8ac8318362f36ac5a50c21a6174d8244b7cf8e20
Jon <audiobahn404@gmail.com>
2016-09-14 00:03:41 -0400

    Limit number of Events shown on the home page As the number of users increases, and as time goes on, it's easily possible for thousands of Events to show up in a User's timeline. Obviously, this would be quite a burden on the server, the network, and the client. So, the number of displayed events has been arbitrarily limited to 100.
    This should be replaced by a pagination system (and probably lazy loading) at some point.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,9 @@
 class EventsController < ApplicationController
   def index
     if current_user
-      @events = Event.stream_for_user(current_user)
+      @events = Event.stream_for_user(current_user).limit(100)
     else
-      @events = Event.all
+      @events = Event.limit(100)
     end
   end
 end
